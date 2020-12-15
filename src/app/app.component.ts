e470398acb2d1adb8140c4d8d0452c54d010d1e3
Bastien CLEMENT <bclement@assurone-group.com>
2020-12-15 11:07:07 +0100

    style composant navbar.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appbook';
+  bgColor = '#5633c3';
 }
--- src/app/areas/book-lists/components/book-lists.component.ts
+++ src/app/areas/book-lists/components/book-lists.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-book-lists',
   templateUrl: './book-lists.component.html'
 })
 
-export class BookListsComponent{ }
+export class BookListsComponent{
+  @Input() bgColor: string;
+}

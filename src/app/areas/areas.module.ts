830ca85121bd8934a63eb711a744baaa200374f6
Bastien CLEMENT <bclement@assurone-group.com>
2020-12-15 10:32:48 +0100

    ajout composant book lists

---
+++ src/app/areas/areas.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {BookListsComponent} from './book-lists/components/book-lists.component';
+
+@NgModule({
+  declarations: [BookListsComponent],
+  imports: [],
+  exports: [BookListsComponent]
+})
+
+export class AreasModule { }
---
+++ src/app/areas/book-lists/components/book-lists.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-book-lists',
+  templateUrl: './book-lists.component.html'
+})
+
+export class BookListsComponent{ }
